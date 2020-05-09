44dcb14fc4d2924198e04a5873163775cf384cb6
Anita John <anitaj1@learn.cssd.ab.ca>
2020-05-09 14:47:55 -0600

    Added ball image 1, centered it to the middle, and made it fit the screen.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.black12,
+          appBar: AppBar(
+            title: Text('Magic 8 Ball'),
+            backgroundColor: Colors.black12,
+          ),
+           body: MagicBall()
+        )
       ),
     );
+class MagicBall extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Image.asset('images.ball1.png'),
+        ),
+      ],
+    );
+  }
+}
+
